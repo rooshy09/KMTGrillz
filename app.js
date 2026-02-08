387e36b515318e1af5a8084be5ef34e6e6aca5fa
rooshy09 <rushivayeda@gmail.com>
2026-02-08 11:42:13 +1100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+/*NAVBAR DROP DOWN 
+const dropdown = document.querySelector(".navdropdown");
+const btn = dropdown.querySelector(".navdropdown__btn");
+
+btn.addEventListener("click", () => {
+  dropdown.classList.toggle("is-open");
+});
+
+*/
